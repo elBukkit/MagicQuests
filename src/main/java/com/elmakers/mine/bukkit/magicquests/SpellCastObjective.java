c5509ad0a9e923b6c5ac22d169bc24991aa582f3
Nathan Wolf <nathan@elmakers.com>
2015-11-12 05:54:55 -0800

    Add a lot of debug info to the cast objective ... it's just not firing sometimes :(

--- src/main/java/com/elmakers/mine/bukkit/magicquests/SpellCastObjective.java
+++ src/main/java/com/elmakers/mine/bukkit/magicquests/SpellCastObjective.java
@@ -1,12 +1,14 @@
 package com.elmakers.mine.bukkit.magicquests;
 
 import com.elmakers.mine.bukkit.api.event.CastEvent;
+import com.elmakers.mine.bukkit.api.magic.Mage;
 import com.elmakers.mine.bukkit.api.spell.Spell;
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
 import me.blackvein.quests.CustomObjective;
 import me.blackvein.quests.Quest;
 import me.blackvein.quests.Quester;
 import me.blackvein.quests.Quests;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 
@@ -21,12 +23,17 @@ public class SpellCastObjective extends CustomObjective {
         this.setShowCount(true);
         this.addData("Spell");
         this.setCountPrompt("Enter the number of times the player must cast the spell:");
-        this.addDescription("Spell", "The name of the Magic spell the player must have cast.");
+        this.addDescription("Spell", "The name of the Magic spell the player must cast.");
         this.setDisplay("Cast %Spell%: %count%");
     }
 
     @EventHandler
     public void onSpellCast(CastEvent event) {
+        Mage mage = event.getMage();
+
+        mage.sendDebugMessage(ChatColor.GRAY + "Checking spell cast objective for cast of " + ChatColor.GOLD + event.getSpell().getName() +
+                ChatColor.BLUE + " with result of " + ChatColor.YELLOW + event.getSpellResult());
+
         Player player = event.getMage().getPlayer();
         if (player == null) return;
 
@@ -40,6 +47,10 @@ public class SpellCastObjective extends CustomObjective {
             return;
         }
 
+        mage.sendDebugMessage(ChatColor.BLUE + "Checking spell cast objective for cast of " + ChatColor.GOLD + event.getSpell().getName() +
+                ChatColor.BLUE + " with " + ChatColor.YELLOW + quester.currentQuests.size() +
+                ChatColor.BLUE + " active quests", 5);
+
         for (Quest quest : quester.currentQuests.keySet()) {
             Map<String, Object> map = null;
             try {
@@ -47,14 +58,21 @@ public class SpellCastObjective extends CustomObjective {
             } catch (Exception ex) {
                 map = null;
             }
-            if (map == null) continue;
+            if (map == null) {
+                mage.sendDebugMessage("No active data for this objective for quest: " + ChatColor.GOLD + quest.getName(), 5);
+                continue;
+            }
 
             String spellName = (String)map.get("Spell");
-            if (spellName == null) return;
+            if (spellName == null) {
+                mage.sendDebugMessage(ChatColor.RED + "No active data for this objective for quest: " + ChatColor.GOLD + quest.getName(), 5);
+                return;
+            }
 
             Spell spell = event.getSpell();
-            if (!spellName.equalsIgnoreCase(spell.getName()) && !spellName.equalsIgnoreCase(spell.getKey())) return;
+            if (!spellName.equalsIgnoreCase(spell.getName()) && !spellName.equalsIgnoreCase(spell.getKey())) break;
 
+            mage.sendDebugMessage(ChatColor.GREEN + "Incrementing objective for quest: " + ChatColor.GOLD + quest.getName(), 5);
             incrementObjective(player, this, 1, quest);
         }
     }
